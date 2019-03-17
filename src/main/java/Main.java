f807ee314ebc1ad84ed14829ae1b26ad536acff7
ivaylostanev <ivailo.s.stanev@gmail.com>
2019-03-17 19:18:58 +0200

    Properties file now serving properties to bean correctly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@
 
 import com.springinaction.springidol.Performer;
 import com.springinaction.springidol.impl.Instrumentalist;
+import com.springinaction.springidol.impl.Ticket;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,10 +11,14 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Performer performer = context.getBean("Carl", Instrumentalist.class);
+/*
+        Performer performer = context.getBean("kenny", Instrumentalist.class);
         performer.perform();
+*/
 
-
+        Ticket ticket = context.getBean("ticket", Ticket.class);
+        System.out.println(ticket.getCat());
+        //System.out.println(ticket.getOwner2());
 
 
 //        System.out.println(context.getBeanDefinitionCount());
--- src/main/java/com/springinaction/springidol/impl/Ticket.java
+++ src/main/java/com/springinaction/springidol/impl/Ticket.java
@@ -1,4 +1,35 @@
 package com.springinaction.springidol.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Ticket {
+
+    @Autowired
+    Environment env;
+
+    @Value("${some.key}")
+    private String owner;
+
+    private String cat;
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
 }
